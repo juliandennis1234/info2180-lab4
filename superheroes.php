bcb870463b7ba919f0a2c3eed2cbed515b347efd
Julian Dennis <juliandennis1234@gmail.com>
2025-11-13 11:44:17 -0500

    Implemented superhero search functionality in superheroes.php

--- superheroes.php
+++ superheroes.php
@@ -1,72 +1,48 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
+// Define superheroes as an array of associative arrays
 $superheroes = [
-  [
-      "id" => 1,
-      "name" => "Steve Rogers",
-      "alias" => "Captain America",
-      "biography" => "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.",
-  ],
-  [
-      "id" => 2,
-      "name" => "Tony Stark",
-      "alias" => "Ironman",
-      "biography" => "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man.",
-  ],
-  [
-      "id" => 3,
-      "name" => "Peter Parker",
-      "alias" => "Spiderman",
-      "biography" => "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
-  ],
-  [
-      "id" => 4,
-      "name" => "Carol Danvers",
-      "alias" => "Captain Marvel",
-      "biography" => "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
-  ],
-  [
-      "id" => 5,
-      "name" => "Natasha Romanov",
-      "alias" => "Black Widow",
-      "biography" => "Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers.",
-  ],
-  [
-      "id" => 6,
-      "name" => "Bruce Banner",
-      "alias" => "Hulk",
-      "biography" => "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.",
-  ],
-  [
-      "id" => 7,
-      "name" => "Clint Barton",
-      "alias" => "Hawkeye",
-      "biography" => "A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer.",
-  ],
-  [
-      "id" => 8,
-      "name" => "T'challa",
-      "alias" => "Black Panther",
-      "biography" => "T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.",
-  ],
-  [
-      "id" => 9,
-      "name" => "Thor Odinson",
-      "alias" => "Thor",
-      "biography" => "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.",
-  ],
-  [
-      "id" => 10,
-      "name" => "Wanda Maximoff",
-      "alias" => "Scarlett Witch",
-      "biography" => "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world.",
-  ], 
+  ["name" => "Captain America", "alias" => "Steve Rogers", "biography" => "Super soldier and leader of the Avengers."],
+  ["name" => "Ironman", "alias" => "Tony Stark", "biography" => "Genius billionaire inventor in a metal suit."],
+  ["name" => "Spiderman", "alias" => "Peter Parker", "biography" => "Teenager with spider-like abilities."],
+  ["name" => "Captain Marvel", "alias" => "Carol Danvers", "biography" => "Powerful cosmic hero with photon blasts."],
+  ["name" => "Black Widow", "alias" => "Natasha Romanoff", "biography" => "Expert spy and skilled martial artist."],
+  ["name" => "Hulk", "alias" => "Bruce Banner", "biography" => "Scientist who transforms into a green powerhouse."],
+  ["name" => "Hawkeye", "alias" => "Clint Barton", "biography" => "Master archer with unmatched accuracy."],
+  ["name" => "Black Panther", "alias" => "T'Challa", "biography" => "King of Wakanda with enhanced strength."],
+  ["name" => "Thor", "alias" => "Thor Odinson", "biography" => "Norse God of Thunder who wields Mjolnir."],
+  ["name" => "Scarlett Witch", "alias" => "Wanda Maximoff", "biography" => "Reality-bending Avenger with chaos magic."]
 ];
 
-?>
+// Check if a query is passed
+$query = isset($_GET['query']) ? trim($_GET['query']) : "";
 
-<ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
+// If no query → show all heroes in a list
+if ($query === "") {
+  echo "<ul>";
+  foreach ($superheroes as $hero) {
+    echo "<li>{$hero['name']}</li>";
+  }
+  echo "</ul>";
+  exit;
+}
+
+// Otherwise → search for a match (by name or alias)
+$found = null;
+foreach ($superheroes as $hero) {
+  if (strcasecmp($hero['name'], $query) == 0 || strcasecmp($hero['alias'], $query) == 0) {
+    $found = $hero;
+    break;
+  }
+}
+
+// Output result
+if ($found) {
+  echo "<h3>{$found['name']}</h3>";
+  echo "<h4>A.K.A {$found['alias']}</h4>";
+  echo "<p>{$found['biography']}</p>";
+} else {
+  echo "<p>Superhero not found</p>";
+}
+?>
